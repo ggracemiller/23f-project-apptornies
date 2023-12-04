2d395c94a1945acd1ff20f2ef0ca6401383faed0
Grace Miller <miller.gra@northeastern.edu>
2023-12-04 14:30:16 -0500

    added route to get only attornies

--- flask-app/src/employees/employees.py
+++ flask-app/src/employees/employees.py
@@ -12,7 +12,7 @@ def getValString(val):
     else:
       return '"' + str(val) + '"'
 
-# Get all the products from the database
+# Get all the employees from the database
 @employees.route('/employees', methods=['GET'])
 def get_employees():
     # get a cursor object from the database
@@ -20,6 +20,32 @@ def get_employees():
 
     # use cursor to query the database for a list of products
     cursor.execute('SELECT * FROM employee')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+# Get all the attornies from the database
+@employees.route('/attornies', methods=['GET'])
+def get_employees():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('SELECT * FROM employee WHERE employee_type = \'attorney\'')
 
     # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
